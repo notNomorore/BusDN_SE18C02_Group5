6757575858a32296867485b98ead5bac6181a490
Nhat Minh <nguyennhatminhnau@gmail.com>
2026-02-24 22:54:41 +0700

    Add usecase 'Login with Gmail'

--- BusDN_Backend/config/passport.js
+++ BusDN_Backend/config/passport.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const { User } = require('../models/models');
-
+require('dotenv').config();
 module.exports = (app) => {
     // --- PASSPORT GOOGLE STRATEGY ---
     passport.use(new GoogleStrategy({
